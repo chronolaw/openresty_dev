54e1d65121efb48e864b13ce52f82d3615a98739
chronolaw <chrono_cpp@me.com>
2016-07-05 15:29:21 +0800

    service

---
+++ service/conf/common.lua
@@ -0,0 +1,16 @@
+-- Copyright (C) 2016 by chrono
+
+local proto = { _VERSION = '0.01' }
+
+-- below are configs
+
+proto.version = '0.01'
+
+proto.max_count = 100
+
+proto.time_out = 300
+
+-- above are configs
+
+return proto
+
