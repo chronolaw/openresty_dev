c114a7ee29e414f8cf51e52fe71fcc04c32464ed
chronolaw <chrono_cpp@me.com>
2017-01-01 20:33:36 +0800

    setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 echo "create directories for nginx ..."
 
 #dirs="client_body_temp logs"
-dirs="client_body_temp fastcgi_temp logs proxy_temp scgi_temp uwsgi_temp"
+#$dirs="client_body_temp fastcgi_temp logs proxy_temp scgi_temp uwsgi_temp"
+dirs="logs"
 
 for name in $dirs
 do
