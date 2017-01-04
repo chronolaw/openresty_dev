176852fa1d754694bf4d8e93a5a8b6c050d4f432
chronolaw <chrono_cpp@me.com>
2017-01-04 12:43:53 +0800

    type.lua

---
+++ lua/type.lua
@@ -0,0 +1,10 @@
+#!/usr/local/openresty/bin/resty
+
+print(type(nil))            -- nil
+print(type(true))           -- boolean
+print(type(42))             -- number
+print(type(2.718))          -- number
+print(type("metroid"))      -- string
+print(type(print))          -- function
+print(type(table))          -- table
+
