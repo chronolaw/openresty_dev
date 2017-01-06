19dbf901462c6f8bf0e0468c75b36f15dde92b93
chronolaw <chrono_cpp@me.com>
2017-01-06 15:50:59 +0800

    ffi.lua

--- lua/ffi.lua
+++ lua/ffi.lua
@@ -6,11 +6,12 @@ local ffi = require "ffi"
 
 local ffi_null = ffi.null
 local ffi_cdef = ffi.cdef
+local ffi_typeof = ffi.typeof
 local ffi_new = ffi.new
 local ffi_C = ffi.C
 
 ffi_cdef[[
-struct timeval {
+struct timeval {                // this is a comment
     long int tv_sec;
     long int tv_usec;
 };
@@ -18,13 +19,14 @@ struct timeval {
 int gettimeofday(struct timeval *tv, void *tz);
 ]]
 
-local tm = ffi_new("struct timeval")
+local timeval_t = ffi_typeof("struct timeval")
+local tm = ffi_new(timeval_t)
 ffi_C.gettimeofday(tm, ffi_null)
 
 --local sec = tonumber(tm.tv_sec)
 --local usec = string.format("%06d", tonumber(tm.tv_usec))
 
-ngx.say(type(tm.tv_sec))
-ngx.say("sec:", tm.tv_sec, " usec:", tm.tv_usec)
-ngx.say("sec:", tonumber(tm.tv_sec), " usec:", tonumber(tm.tv_usec))
+print(type(tm.tv_sec))
+print("sec:", tm.tv_sec, " usec:", tm.tv_usec)
+print("sec:", tonumber(tm.tv_sec), " usec:", tonumber(tm.tv_usec))
 
