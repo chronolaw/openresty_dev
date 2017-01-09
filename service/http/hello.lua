b6dfb5fc5b81be299da5a01d0b7f46d2ba035669
chronolaw <chrono_cpp@me.com>
2017-01-09 16:01:50 +0800

    hello ngx_lua

--- service/http/hello.lua
+++ service/http/hello.lua
@@ -1,12 +1,33 @@
 -- Copyright (C) 2016 by chrono
 
-local data = "hello"
+--local data = "hello"
+--ngx.print(data)
 
-ngx.header["Content-length"] = #data
+--ngx.header["Content-length"] = #data
 --ngx.header.content_length = #data
 
-ngx.log(ngx.ERR, "test openresty")
+ngx.log(ngx.INFO, "hello openresty")
+ngx.log(ngx.WARN, "warning is ", "some messgae")
 
-ngx.print(data)
+local print = ngx.say
 
-ngx.exit(200)
+--print(ngx.time())
+--print(ngx.now())
+--print(ngx.today())
+
+local secs = ngx.time()
+local msecs = ngx.now()
+
+assert(msecs - secs < 1)
+
+print(ngx.http_time(secs))
+
+print(ngx.var.uri)
+print(ngx.var['http_host'])
+
+if #ngx.var.is_args > 0 then
+    print(ngx.var.args)
+end
+
+ngx.say('hello openresty')
+--ngx.exit(200)
