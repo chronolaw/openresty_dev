50034cfb71e1afc9481ca5f4b07a32d890d30fc3
chronolaw <chrono_cpp@me.com>
2017-01-11 12:39:07 +0800

    stream_lua config

--- config.sh
+++ config.sh
@@ -2,17 +2,22 @@
 
 build_date="${USER} build at `date '+%Y.%m.%d %H:%M'`"
 
-modules="--with-stream"
-
 no_modules="--without-http_fastcgi_module \
             --without-http_uwsgi_module \
             --without-http_scgi_module
            "
 
-addons=""
+stream="--with-stream"
+
+github="${HOME}/github"
+
+addons="--add-module=${github}/stream-lua-nginx-module
+       "
 
 ./configure     \
         --build="${build_date}" \
-        ${no_modules}
+        ${no_modules}           \
+        ${stream}               \
+        ${addons}
 
 
