be28274f4a45a6070b488ba2be5a686c59bbb90a
chronolaw <chrono_cpp@me.com>
2017-12-26 14:41:47 +0800

    resty.core in stream_lua

--- service/stream/init.lua
+++ service/stream/init.lua
@@ -1,5 +1,12 @@
 -- Copyright (C) 2017 by chrono
 
+if not ngx.config
+    or not ngx.config.ngx_lua_version
+    or ngx.config.ngx_lua_version < 4
+then
+    return
+end
+
 require "resty.core"
 collectgarbage("collect")  -- just to collect any garbage
 
