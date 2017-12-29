1fedc1ff53df02766b49452caa4b4cbf45151e13
chronolaw <chrono_cpp@me.com>
2017-12-29 13:22:25 +0800

    cjson.lua

---
+++ service/http/cjson.lua
@@ -0,0 +1,31 @@
+-- Copyright (C) 2017 by chrono
+
+-- safe decode
+local cjson = require "cjson.safe"
+
+local str = cjson.encode({name='jojo', cat = 'comic'})
+
+ngx.say(str)
+
+local obj = cjson.decode(str)
+
+assert(obj.name == 'jojo')
+assert(obj.cat == 'comic')
+
+obj = cjson.decode([[{"error":format"}]])
+assert(not obj)
+
+str = cjson.encode({1,2,3,4})
+ngx.say(str)
+
+cjson.encode_empty_table_as_object(false)
+str = cjson.encode({})
+ngx.say(str)
+
+cjson.encode_empty_table_as_object(true)
+str = cjson.encode({a={},b=cjson.empty_array})
+ngx.say(str)
+
+cjson.encode_number_precision(5)
+str = cjson.encode({x=math.pi})
+ngx.say(str)
