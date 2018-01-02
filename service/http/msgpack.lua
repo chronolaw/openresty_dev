732b07656a13268f6f36fbe8ecb6ddff736fa06e
chronolaw <chrono_cpp@me.com>
2018-01-02 16:07:58 +0800

    msgpack.lua

---
+++ service/http/msgpack.lua
@@ -0,0 +1,28 @@
+-- Copyright (C) 2017 by chrono
+
+-- local mp = require "resty.msgpack"
+local ok, mp = pcall(require, "resty.msgpack")
+if not ok then
+    ngx.say("resty.msgpack has not been installed")
+    return
+end
+
+local obj = {a=1, b=2, x="star"}
+
+local enc = mp.pack(obj)
+ngx.say("encode len = ", #enc)
+
+local dec = mp.unpack(enc)
+
+assert(dec.a == 1 and dec.b == 2)
+assert(dec.x == obj.x)
+
+local a = 'hello'
+local b = {1,2,3}
+local c = 2.718
+
+local data = mp.pack(a)..mp.pack(b)..mp.pack(c)
+
+for i, v in mp.unpacker(data) do
+    ngx.say("offset ", i, " is ", v)
+end
