a962a88360f0fb13ea3fbd4a1038fcda17212df9
chronolaw <chrono_cpp@me.com>
2018-01-24 14:56:14 +0800

    backend.conf

---
+++ service/backend/capture.lua
@@ -0,0 +1,22 @@
+-- Copyright (C) 2018 by chrono
+
+-- curl 127.1:81/capture
+
+ngx.say('now capture proxy_pass:\n')
+
+local res = ngx.location.capture('/hello')
+
+if res.status ~= ngx.HTTP_OK then
+    ngx.exit(res.status)
+end
+
+ngx.print(res.body)
+
+
+ngx.say('\nnow capture redis2_pass:\n')
+
+local res = ngx.location.capture(
+                '/ngx_redis2',
+                {args = {key='metroid'}})
+
+ngx.print(res.body)
