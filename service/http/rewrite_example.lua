132ef55f14e7cef3f0e9e2d244837aeac36688a0
chronolaw <chrono_cpp@me.com>
2018-01-24 12:14:03 +0800

    rewrite_example.lua

--- service/http/rewrite_example.lua
+++ service/http/rewrite_example.lua
@@ -3,6 +3,13 @@
 local method = ngx.req.get_method()
 if method ~= 'GET' and
    method ~= 'POST' then
+
+   ngx.header['Allow'] = 'GET, POST'
+   ngx.exit(405)
+end
+
+local ver = ngx.req.http_version()
+if ver < 1.1 then
    ngx.exit(400)
 end
 
