6b5d3ba21a9e7cbbbfd8854f8f6b9424332f9d57
chronolaw <chrono_cpp@me.com>
2018-01-24 13:53:17 +0800

    access_example.lua

--- service/http/access_example.lua
+++ service/http/access_example.lua
@@ -1,10 +1,10 @@
 -- Copyright (C) 2018 by chrono
 
-local black_list = {}
+--local black_list = {}
 local white_list = {['127.0.0.1']=1}
 
 local ip = ngx.var.remote_addr
-if black_list[ip] or
+if --black_list[ip] or
    not white_list[ip] then
     ngx.log(ngx.ERR, ip, " is blocked")
     ngx.exit(403)
