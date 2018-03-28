28443e38c6e6acc49201db4ab35c444463b212bb
chronolaw <chrono_cpp@me.com>
2018-03-28 09:34:55 +0800

    base64.lua

--- service/http/base64.lua
+++ service/http/base64.lua
@@ -9,3 +9,18 @@ ngx.say(dec)
 
 local enc = ngx.encode_base64(str, true)
 ngx.say(enc)
+
+local ok, b64 = pcall(require, "ngx.base64")
+if not ok then
+    ngx.say('no base64url')
+    return
+end
+
+local str = "=>?@"
+local enc_std = ngx.encode_base64(str)
+local enc_url = b64.encode_base64url(str)
+local dec = b64.decode_base64url(enc_url)
+
+ngx.say(enc_std)
+ngx.say(enc_url)
+assert(dec == str)
