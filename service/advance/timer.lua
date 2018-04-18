57e6341359543ee2ecb377417d6826a3f709affa
chronolaw <chrono_cpp@me.com>
2018-04-18 15:11:51 +0800

    timer.lua

---
+++ service/advance/timer.lua
@@ -0,0 +1,41 @@
+-- Copyright (C) 2017-2018 by chrono
+
+local function once_task(premuture, uri)
+    if premuture then
+        ngx.log(ngx.WARN, uri, ": task abort")
+        return
+    end
+
+    local http = require "resty.http"
+    local httpc = http:new()
+
+    local res, err = httpc:request_uri('http://127.0.0.1/hello')
+
+    ngx.log(ngx.ERR, uri, ": task success")
+end
+
+local ok, err = ngx.timer.at(0.1, once_task, ngx.var.uri)
+if not ok then
+    ngx.say("timer failed: ", err)
+    return
+end
+
+local function min_task(premuture, name)
+    if premuture then
+        return
+    end
+
+    ngx.shared[name]:incr("count", 1, 0)
+end
+
+--[[
+local ok, err = ngx.timer.every(60, min_task, "shmem")
+if not ok then
+    ngx.say("timer failed: ", err)
+    return
+end
+--]]
+
+ngx.say('hello openresty timer')
+
+
