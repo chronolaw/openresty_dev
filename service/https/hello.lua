1c825ff80729a79e44d477e20d19633cc8319290
chronolaw <chrono_cpp@me.com>
2018-04-19 16:42:52 +0800

    hello https

--- service/https/hello.lua
+++ service/https/hello.lua
@@ -1,3 +1,22 @@
 -- Copyright (C) 2017-2018 by chrono
 
-ngx.say('hello openresty in https')
+local ssl = require "ngx.ssl"
+
+ngx.say("scheme: ", ngx.var.scheme)
+
+local name, err = ssl.server_name()
+ngx.say("sni: ", name)
+
+local ver, err = ssl.get_tls1_version()
+ngx.say("ver:", ver)
+
+local ver, err = ssl.get_tls1_version_str()
+ngx.say("ver:", ver)
+
+--local addr, addrtyp, err = ssl.raw_client_addr()
+--ngx.say("client addr: ", addrtyp, addr)
+
+local addr, addrtyp, err = ssl.raw_server_addr()
+ngx.say("server addr: ", addrtyp, addr)
+
+ngx.say('hello openresty')
