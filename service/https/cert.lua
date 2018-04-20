3c3f4bf6cadafd2dce2c5084b999dd91c18584d0
chronolaw <chrono_cpp@me.com>
2018-04-20 10:39:12 +0800

    ssl_certificate_by_lua

--- service/https/cert.lua
+++ service/https/cert.lua
@@ -1,6 +1,18 @@
 -- Copyright (C) 2017-2018 by chrono
 
+-- curl -k https://www.chrono.com:84/hello
+-- curl -k https://www.metroid.net:84/hello
+
 local ssl = require "ngx.ssl"
+
+local phase = ngx.get_phase()
+if phase ~= "ssl_cert" then
+    ngx.log(ngx.ERR, "ssl can't run in ", phase)
+    ngx.exec("/")
+    return
+end
+
+ngx.log(ngx.ERR, "enter ssl_certificate_by_lua")
 
 local function get_pem(file)
     local f = io.open(file, 'r')
@@ -20,20 +32,26 @@ local certs = {
     }
 
 local function init_certs()
-    local prefix = ngx.config.prefix().."/conf/ssl/"
+    local prefix = ngx.config.prefix().."conf/ssl/"
 
     for k,_ in pairs(certs) do
         local name = certs[k].name
 
         certs[k].cert = get_pem(prefix..name..'.crt')
         certs[k].key = get_pem(prefix..name..'.key')
+
+        ngx.log(ngx.ERR, "cert=", #certs[k].cert)
+        ngx.log(ngx.ERR, "key=", #certs[k].key)
     end
 end
+
+-- read certs from disk or redis ...
+init_certs()
 
 local name, err = ssl.server_name()
 if not name then
     ngx.log(ngx.ERR, "no SNI found: ", err)
-    return ngx.exit(ngx.ERROR)
+    ngx.exit(ngx.ERROR)
 end
 
 --ngx.ctx.sni = name
@@ -41,5 +59,30 @@ end
 local ok, err = ssl.clear_certs()
 if not ok then
     ngx.log(ngx.ERR, "clear cert failed: ", err)
-    return ngx.exit(ngx.ERROR)
+    ngx.exit(ngx.ERROR)
 end
+
+local cert, err = ssl.parse_pem_cert(certs[name].cert)
+if not cert then
+    ngx.log(ngx.ERR, "failed to parse cert: " , err)
+    ngx.exit(ngx.ERROR)
+end
+
+local ok, err = ssl.set_cert(cert)
+if not ok then
+    ngx.log(ngx.ERR, "failed to set cert: " , err)
+    ngx.exit(ngx.ERROR)
+end
+
+local key, err = ssl.parse_pem_priv_key(certs[name].key)
+if not key then
+    ngx.log(ngx.ERR, "failed to parse key: " , err)
+    ngx.exit(ngx.ERROR)
+end
+
+local ok, err = ssl.set_priv_key(key)
+if not ok then
+    ngx.log(ngx.ERR, "failed to set key: " , err)
+    ngx.exit(ngx.ERROR)
+end
+
