9eb8d90b39d82d95ad5441fdb3d113503b281df3
chronolaw <chrono_cpp@me.com>
2018-04-20 17:49:23 +0800

    https hello

--- service/https/hello.lua
+++ service/https/hello.lua
@@ -1,6 +1,12 @@
 -- Copyright (C) 2017-2018 by chrono
 
 local ssl = require "ngx.ssl"
+local byte = string.byte
+
+local function addr_str(addr)
+    return string.format("%d.%d.%d.%d", byte(addr, 1), byte(addr, 2),
+            byte(addr, 3), byte(addr, 4))
+end
 
 ngx.say("scheme: ", ngx.var.scheme)
 
@@ -8,15 +14,18 @@ local name, err = ssl.server_name()
 ngx.say("sni: ", name)
 
 local ver, err = ssl.get_tls1_version()
-ngx.say("ver:", ver)
+ngx.say("ver: ", ver)
 
 local ver, err = ssl.get_tls1_version_str()
-ngx.say("ver:", ver)
+ngx.say("ver: ", ver)
+
+ngx.say("ssl_cipher: ", ngx.var.ssl_cipher)
+ngx.say("ssl_ciphers: ", ngx.var.ssl_ciphers)
 
 --local addr, addrtyp, err = ssl.raw_client_addr()
 --ngx.say("client addr: ", addrtyp, addr)
 
 local addr, addrtyp, err = ssl.raw_server_addr()
-ngx.say("server addr: ", addrtyp, addr)
+ngx.say(addrtyp, ": ", addr_str(addr))
 
 ngx.say('hello openresty')
