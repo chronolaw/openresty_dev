f55dee2668120ea31f3f1343ef322941747a5b80
chronolaw <chrono_cpp@me.com>
2018-04-25 16:45:31 +0800

    http2/hello.lua

--- service/http2/hello.lua
+++ service/http2/hello.lua
@@ -1,5 +1,7 @@
 -- Copyright (C) 2017-2018 by chrono
 
+-- curl --http2 -k 'https://127.1:85/hello'
+
 local ssl = require "ngx.ssl"
 
 ngx.say("scheme: ", ngx.var.scheme)
@@ -11,5 +13,9 @@ ngx.say("sni: ", name)
 
 local ver, err = ssl.get_tls1_version_str()
 ngx.say("tls:", ver)
+
+-- h2: http2 with ssl
+-- h2c: http2 with cleartext
+ngx.say(ngx.var.http2)
 
 ngx.say('hello openresty http2')
