7a7a752557314e17e0c5c84c5cfa0701ea6be8e7
chronolaw <chrono_cpp@me.com>
2018-05-18 12:03:33 +0800

    settitle.lua

---
+++ service/http/settitle.lua
@@ -0,0 +1,26 @@
+-- Copyright (C) 2018 by chrono
+
+local ffi = require "ffi"
+
+local ffi_null = ffi.null
+local ffi_cdef = ffi.cdef
+local ffi_typeof = ffi.typeof
+local ffi_new = ffi.new
+local ffi_fill = ffi.fill
+local ffi_copy = ffi.copy
+local ffi_C = ffi.C
+
+ffi_cdef[[
+void ngx_setproctitle(char *title);
+]]
+
+local title = "new title for worker " .. ngx.worker.id()
+
+ngx.say("change title to : ", title)
+
+local str = ffi_new("char[?]", #title + 1)
+
+ffi_fill(str, 0, #title + 1)
+ffi_copy(str, title, #title)
+
+ffi_C.ngx_setproctitle(str)
