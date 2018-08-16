7fa36f4660646ae9b1fcebacfd0bf3422e264dbc
chronolaw <chrono_cpp@me.com>
2018-08-16 16:16:07 +0800

    settitle.lua

--- service/http/settitle.lua
+++ service/http/settitle.lua
@@ -14,13 +14,28 @@ ffi_cdef[[
 void ngx_setproctitle(char *title);
 ]]
 
+local function setproctitle(title)
+
+    local str = ffi_new("char[?]", #title + 1)
+
+    ffi_fill(str, 0, #title + 1)
+    ffi_copy(str, title, #title)
+
+    ffi_C.ngx_setproctitle(str)
+end
+
+-- delay to change title
+local function setproctitle_delay(time, title)
+    ngx.timer.at(time,
+            function(premature)
+                setproctitle(title)
+            end
+            )
+end
+
 local title = "new title for worker " .. ngx.worker.id()
 
 ngx.say("change title to : ", title)
 
-local str = ffi_new("char[?]", #title + 1)
+setproctitle_delay(0.1, title)
 
-ffi_fill(str, 0, #title + 1)
-ffi_copy(str, title, #title)
-
-ffi_C.ngx_setproctitle(str)
